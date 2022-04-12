bb3856ac9663cf842451ec67c91138c5f29800d0
walkenho <walkenho@users.noreply.github.com>
2022-04-12 23:40:35 +0100

    Add first functional version of NatoSpellBot

--- natoSpellBot.py
+++ natoSpellBot.py
@@ -1,62 +1,47 @@
+import json
+
 from dotenv import dotenv_values
 from telegram import ParseMode
 from telegram.ext import Updater, CommandHandler
 import logging
 
+import pathlib
+DATAPATH = pathlib.Path(__file__).parent/'data'
+
 
 def load_nato_dictionary():
-    pass
+    with open(DATAPATH/'natoalphabet.json', 'r') as f:
+        return json.load(f)
 
 
 def start(update, context):
     context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text="Hello, I am a spellBot. I can spell your text "
-        "using the Nato Alphabet. Use \\spell followed by your text "
-        "to ask me to spell.",
+        text="Hello, I am NatoSpellBot, I have international spelling power! I can spell your text "
+        "using the Nato Alphabet. Use /spell followed by your text to tell me what to spell."
     )
 
 
-def source_data():
-    # TODO: Source data from updatable repository/possibly API?
-    data = {}
-    with open("acronyms.csv", "r") as f:
-        lines = f.readlines()
-    for line in lines:
-        entries = line.split(",")
-        if len(entries) != 2:
-            pass
-        else:
-            data[entries[0].upper()] = entries[1]
-    return data
+def nato_spell(tokenlist):
+    nato_dict = load_nato_dictionary()
+    return (
+            ' '.join(tokenlist)
+            + ':\n'
+            + '\n'.join([word + ' - ' + " ".join([nato_dict[letter] for letter in word.upper()]) for word in tokenlist])
+    )
 
 
-def generate_response(context):
-    if len(context.args) != 1:
-        return "Please specify a single acronym."
-
-    acronym = context.args[0]
-
-    data = source_data()
-
-    try:
-        return f"{acronym} stands for {data[acronym.upper()]}"
-    except KeyError:
-        return (
-            f"Sorry, I don't understand this acronym neither. "
-            f"Maybe try <a href='https://www.google.com/search?q={acronym}'>here</a>?"
-        )
-
-
-def query(update, context):
-    response = generate_response(context)
+def generate_response(update, context):
+    response = nato_spell(context.args)
+    print(update.chat_member)
+    print(update.effective_user)
     context.bot.send_message(
         chat_id=update.effective_chat.id, text=response, parse_mode=ParseMode.HTML
     )
 
 
 def run_bot():
-    token = dotenv_values()["token"]
+    token = dotenv_values('.env')["TOKEN"]
 
     updater = Updater(token=token, use_context=True)
     # define shortcut to dispatcher
@@ -65,7 +50,7 @@ def run_bot():
     start_handler = CommandHandler("start", start)
     dispatcher.add_handler(start_handler)
 
-    query_handler = CommandHandler("q", query)
+    query_handler = CommandHandler("spell", generate_response)
     dispatcher.add_handler(query_handler)
 
     updater.start_polling()
