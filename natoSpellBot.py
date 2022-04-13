f3ddb570cc7f15cb805dddc55c95f0cb44e0d58c
walkenho <walkenho@users.noreply.github.com>
2022-04-13 22:25:08 +0100

    Add check if letter is a letter

--- natoSpellBot.py
+++ natoSpellBot.py
@@ -14,14 +14,6 @@ def load_nato_dictionary():
         return json.load(f)
 
 
-def start(update, context):
-    context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="Hello, I am NatoSpellBot, I have international spelling power! I can spell your text "
-        "using the Nato Alphabet. Use /spell followed by your text to tell me what to spell.",
-    )
-
-
 def nato_spell(tokenlist):
     nato_dict = load_nato_dictionary()
     return (
@@ -29,7 +21,7 @@ def nato_spell(tokenlist):
         + ":\n"
         + "\n".join(
             [
-                word + " - " + " ".join([nato_dict[letter] for letter in word.upper()])
+                word + " - " + " ".join([nato_dict[letter.upper()] if letter.isalpha() else letter for letter in word])
                 for word in tokenlist
             ]
         )
@@ -38,10 +30,18 @@ def nato_spell(tokenlist):
 
 def generate_response(update, context):
     response = nato_spell(context.args)
-    print(update.chat_member)
-    print(update.effective_user)
+    # TODO: convert this into something that can be used to spell somebody's full name and user name
+    # print(update.effective_user)
     context.bot.send_message(
         chat_id=update.effective_chat.id, text=response, parse_mode=ParseMode.HTML
+    )
+
+
+def start(update, context):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Hello, I am NatoSpellBot, I have international spelling power! I can spell your text "
+        "using the Nato Alphabet. Use /spell followed by your text to tell me what to spell.",
     )
 
 
